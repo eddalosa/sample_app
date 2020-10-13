95637deff34bc4be3c45c9049862a92c29f0368b
eddalosa <edmondalosa3@gmail.com>
2020-10-13 16:59:46 +0300

    Responsive header and contacts

--- Gemfile
+++ Gemfile
@@ -4,12 +4,15 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 gem 'rails',      '6.0.2.1'
 gem 'bcrypt',      '3.1.13'
 gem 'puma',       '4.3.3'
-gem 'sass-rails', '5.1.0'
-gem 'bootstrap-sass', '3.4.1'
+gem 'sass-rails'
+gem 'bootstrap-sass'
 gem 'webpacker',  '4.0.7'
 gem 'turbolinks', '5.2.0'
 gem 'jbuilder',   '2.9.1'
 gem 'bootsnap',   '1.4.5', require: false
+gem 'mini_racer'
+
+
 
 group :development, :test do
   gem 'sqlite3', '1.4.1'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,9 @@ require("channels")
 require("jquery")
 
 import "bootstrap"
+import "../stylesheets/application.scss"
 import "@fortawesome/fontawesome-free/js/all";
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,3 +20,5 @@ import "@fortawesome/fontawesome-free/js/all";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
