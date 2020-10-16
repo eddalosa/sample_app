e83208404ba7aaf9f726bcaa62979981226fccac
eddalosa <edmondalosa3@gmail.com>
2020-10-16 15:54:02 +0300

    Add the core values area.

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 	test "full title helper" do 
-		assert_equal full_title,		 "Ruby on Rails Tutorial Sample App"
-		assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+		assert_equal full_title,		 "PEFA Church Kayole B"
+		assert_equal full_title("Help"), "Help | PEFA Church Kayole B"
 	end
 end
