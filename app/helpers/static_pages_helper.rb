acfd00ad1cd47f82606b86d657dd9d3a164ddea3
eddalosa <edmondalosa3@gmail.com>
2021-03-04 00:10:24 +0300

    fixing fetch

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,18 +1,18 @@
 module StaticPagesHelper
 	def blog_helper(index)
     @blogs = Blog.order('created_at DESC').first(1)
-    @blog = @blogs.fetch(index)
+    @blog = @blogs.fetch(index, "No blog found")
   end
 
   def blog_title(index)
     @blogs = Blog.order('created_at DESC').first(1)
-    @blog = @blogs.fetch(index)
+    @blog = @blogs.fetch(index, "No blog found")
     @blog.title
   end
 
   def blog_text_teaser(index)
     @blogs = Blog.order('created_at DESC').first(4)
-    @blog = @blogs.fetch(index)
+    @blog = @blogs.fetch(index, "No blog found")
     truncate(strip_tags(@blog.content.to_s), length: 300, separator: " ")
   end
 end
