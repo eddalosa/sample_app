8a42f709c496dafa3ca2614fd24fd99dd9d8b446
eddalosa <edmondalosa3@gmail.com>
2021-03-04 10:03:46 +0300

    fix blog display

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -6,13 +6,17 @@ module StaticPagesHelper
 
   def blog_title(index)
     @blogs = Blog.order('created_at DESC').first(1)
-    @blog = @blogs.fetch(index, "No blog found")
-    @blog.title
+    if @blogs.any?
+      @blog = @blogs.fetch(index, "No blog found")
+      @blog.title
+    end
   end
 
   def blog_text_teaser(index)
     @blogs = Blog.order('created_at DESC').first(4)
-    @blog = @blogs.fetch(index, "No blog found")
-    truncate(strip_tags(@blog.content.to_s), length: 300, separator: " ")
+    if @blogs.any?
+      @blog = @blogs.fetch(index, "No blog found")
+      truncate(strip_tags(@blog.content.to_s), length: 300, separator: " ")
+    end
   end
 end
